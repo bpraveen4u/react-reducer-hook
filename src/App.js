491dca26d1d5e58e2150a4978026f1ab981c5b7b
Praveen Kumar Bonakurthi <prbona@microsoft.com>
2020-04-23 09:39:58 +0530

    react reducer hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import CounterOne from './components/CounterOne'
+import CounterTwo from './components/CounterTwo'
+import CounterThree from './components/CounterThree'
+import ComponentA from './components/ComponentA'
+import ComponentB from './components/ComponentB'
+import ComponentC from './components/ComponentC'
+import DataFetchingOne from './components/DataFetchingOne'
+import DataFetchingTwo from './components/DataFetchingTwo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const CountContext = React.createContext()
+
+const initialState = 0
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    case 'reset':
+      return initialState
+    default:
+      return state
+  }
 }
 
-export default App;
+function App() {
+  const [count, dispach] = useReducer(reducer, initialState)
+  return (
+    <div>
+      <DataFetchingTwo />
+      {/* <DataFetchingOne /> */}
+      {/* <CountContext.Provider
+      value={{ countState: count, countDispatch: dispach }}
+    >
+      <div className='App'>
+        <h2>count - {count}</h2>
+        <ComponentA />
+        <ComponentB />
+        <ComponentC /> */}
+      {/* <CounterThree /> */}
+      {/* <CounterTwo /> */}
+      {/* <CounterOne /> */}
+      {/* </div>
+    </CountContext.Provider> */}
+    </div>
+  )
+}
+
+export default App
